62c99435f0f80616bc6790dcdf862fa4d26a28eb
Diogo <diogofilipebotelho@gmail.com>
2021-09-07 19:38:52 -0400

    Completed SpecialWordFinder

--- serial.py
+++ serial.py
@@ -18,15 +18,14 @@ class SerialGenerator:
     100
     """
 
-    def __init__(self,start=100):
+    def __init__(self,start=0):
         """Initialize Serial Generator"""
-        self.start = start - 1
-        self.serial = start - 1
+        self.start = self.serial = start
 
     def generate(self):
         """Increment serial number by 1, starting at start"""
         self.serial += 1
-        return self.serial
+        return self.serial - 1
 
     def reset(self):
         """Reset serial number to start value"""
--- wordfinder.py
+++ wordfinder.py
@@ -5,24 +5,42 @@ class WordFinder:
 
     def __init__(self,file_path):
         """Initialize wordfinder with file path and list of words"""
-        self.file_path = file_path
-        self.file_words = self.get_words()
+        #self.file_path = file_path
+        file = open(file_path, 'r')
+        self.words = self.get_words(file)
+        #Print a message that we did this correctly
 
     def get_words(self):
         """creates list of words from file"""
-        file = open(self.file_path, 'r')
-        file_words=[]
-
-        for line in file:
-            if "\n" in line:
-                file_words.append(line[:-1])
-            else:
-                file_words.append(line)
-        return file_words
+        return [line.strip() for line in file] #Elie's way
+        
+        #Our way:
+        # words = []
+        # for line in file:
+        #     if "\n" in line:
+        #         words.append(line[:-1])
+        #     else:
+        #         words.append(line)
+        # return words
 
     def get_random_word(self):
         """gets random word from list of words"""
-        return choice(self.file_words)
+        return choice(self.words)
 
     
-    
+class SpecialWordFinder(WordFinder):
+    """Random Word Finder: Extends WordFinder, ignores empty lines and lines starting with #"""
+    #Import the init of WordFinder
+    #New method goes line by line and has if statement that ignores if the line starts with # or is empty
+    #Option 1: Import the get_words and have a second method to read the words and remove "#" and "" elements
+    #Option 2: Have a new get_words
+    #Import get_random_word method
+
+    # def __init__(self,file_path):
+    #     '''Initializes Special Word Finder with inherited init from WordFinder and runs method trim_file_words'''
+    #     super().__init__(file_path)
+    #     self.trim_file_words()
+
+    def get_words(self,file):
+        '''Removes blank links and # from file'''
+        return [word for word in super().get_words(file) if not (word == "" or word.startswith("#"))]
